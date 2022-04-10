26ade6af1d8162932fed3d529a9ba73615bf8435
Jhonatan Mejia <quenandres@gmail.com>
2022-04-09 19:27:07 -0500

    GET: parámetros query 7/25

--- nodejs/Readme.md
+++ nodejs/Readme.md
@@ -12,3 +12,18 @@ Representaciona State Transfer
 
 
 ### GET: recibir parámetros 6/25
+```
+// El :id es el nombre del parametro que tendra, por lo que con desestructuración se obtiene con el mismo nombre
+app.get('/categories/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'categorie 2',
+    price: 2000
+  });
+});
+```
+
+### GET: parámetros query 7/25
+
+Los endpoints especificos deben ir antes de los endpoint dinamicos.
--- nodejs/backend-api-rest/my-store/index.js
+++ nodejs/backend-api-rest/my-store/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app     = express();
 const port    = 3000;
+const faker   = require('faker');
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
@@ -10,18 +11,26 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  res.json([
-    {
-      name: 'Producto 1',
-      price: 1000
-    },
-    {
-      name: 'Producto 2',
-      price: 2000
-    }
-  ]);
+app.get('/products/filter', (req, res) => {
+  res.send('Yo soy un filter');
 });
+
+app.get('/products', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+
+  }
+  res.json(products);
+});
+
+
 
 app.get('/products/:id', (req, res) => {
   const { id } = req.params;
@@ -65,6 +74,19 @@ app.get('/categories/:cateogryId/products/:productId', (req, res) => {
     price: 2000
   });
 });
+
+
+app.get('/users', (req, res) => {
+  const { limit, offset } = req.query;
+  if( limit && offset ) {
+    res.json({
+      limit,
+      offset
+    });
+  } else {
+    res.send('No hay parametros');
+  }
+}); //Parametro opcional
 
 
 app.listen(port, () => {
