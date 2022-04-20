9f65ba03eb8dbd5ff0d64a17621ab86e297d6758
Jhonatan Mejia <quenandres@gmail.com>
2022-04-20 12:00:53 -0500

    Added a Procfile.

--- nodejs/backend-api-rest/my-store/index.js
+++ nodejs/backend-api-rest/my-store/index.js
@@ -1,6 +1,6 @@
 const express   = require('express');
 const app       = express();
-const port      = 3000;
+const port      = process.env.PORT || 3000;
 const routerApi = require('./routes');
 const cors      = require('cors');
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
@@ -10,7 +10,7 @@ app.use(express.json());
 const whitelist = ['http://localhost:5500', 'http://localhost:3000', 'http://127.0.0.1:5500','http://127.0.0.1:5500/nodejs/backend-api-rest/index.html'];
 const optionsCors = {
   origin: (origin, callback) => {
-    if( whitelist.includes(origin) ) {
+    if( whitelist.includes(origin) || !origin ) { // !origin para especifica el origen asi mismo
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
