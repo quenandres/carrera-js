ac2d4e7d5140938286e69482e2e4fa6877bc0ba8
Jhonatan Mejia <quenandres@gmail.com>
2022-04-29 13:19:52 -0500

    21. Aplicando DRY

--- clean-solid/src/clean-code/05-dry.ts
+++ clean-solid/src/clean-code/05-dry.ts
@@ -6,11 +6,25 @@ class Product {
         public size: Size = ''
     ){}
 
+    isProductReady():boolean{
+        for( const key in this ){
+            switch(typeof this[key]) {
+                case 'string':
+                    if( (<string><unknown>this[key]).length ) throw Error(`${this[key]} is empty`);
+                break;
+                case 'number':
+                    if( (<number><unknown>this[key]) <=0 ) throw Error(`${this[key]} is empty`);
+                break;
+                default:
+                    throw Error(`${ typeof this[key] } is not valid`);
+            }
+        }
+        return true;
+    }
+
     toString() {
-        // No estamos usando DRY
-        if( this.name.length <= 0 ) throw Error('name is empty');
-        if( this.price <= 0 ) throw Error('price is empty');
-        if( this.size.length <= 0 ) throw Error('size is empty');
+        // Usando DRY
+        if( !this.isProductReady ) return;
         return `${this.name} (${this.price}), ${this.size}`;
     }
 }
