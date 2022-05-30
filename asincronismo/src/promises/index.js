0a38f7763bb3c86b3a5cf563546bf8249d2c808c
Jhonatan Mejia <quenandres@gmail.com>
2022-05-30 16:42:06 -0500

    Implementando Promesas 7/12

---
+++ asincronismo/src/promises/index.js
@@ -0,0 +1,41 @@
+const somethingWillHappen = () => {
+    return new Promise((resolve, reject) => {
+        if( true ) {
+            resolve('Hey!!');
+        } else {
+            reject('ups!')
+        }
+    });
+}
+
+somethingWillHappen()
+.then(response => console.log(response)
+.catch(err => console.error(err))
+);
+
+const somethingWillHappen2 = () => {
+    return new Promise((resolve, reject) => {
+        if( true ) {
+            setTimeout(() => {
+                resolve('True');
+            }, 2000);
+        } else {
+            const error = new Error('Wopps !');
+            reject(error);
+        }
+    })
+}
+
+somethingWillHappen2()
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+
+
+Promise.all([somethingWillHappen(), somethingWillHappen2()])
+    .then(response => {
+        console.log('Arrya of results', response);
+    })
+    .catch(err => {
+        console.error(err);
+    })
